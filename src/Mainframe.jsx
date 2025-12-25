bc7c122a74c290fa82904ea21b3f169d31a15927
bobkkblue <chienyec@gmail.com>
2025-12-24 17:05:06 -0800

    add alltrails link to check more detail on the trails

--- src/Mainframe.jsx
+++ src/Mainframe.jsx
@@ -10,15 +10,18 @@ function Mainframe(){
     const choices = [ 
         {   name: "Lake Hollywood Trail",
             file: "LakeHollywood.csv",
-            pos: {lat: 34.129156995834606,lng: -118.33640918218002} //34.129156995834606,-118.33640918218002
+            pos: {lat: 34.129156995834606,lng: -118.33640918218002}, //34.129156995834606,-118.33640918218002
+            alltrails: "https://www.alltrails.com/trail/us/california/lake-hollywood-trail?u=i&sh=sqfqqm"
         },
         {   name: "Hollywood Sign via Canyon Drive",
             file: "CanyonDrive.csv" ,
-            pos: { lat: 34.12545624202727,lng: -118.31386991416954 } //34.12545624202727,-118.31386991416954
+            pos: { lat: 34.12545624202727,lng: -118.31386991416954 }, //34.12545624202727,-118.31386991416954
+            alltrails: "https://www.alltrails.com/trail/us/california/hollywood-sign-via-canyon-drive?u=i&sh=sqfqqm"
         },
         {   name: "Hollywood Sign via Innsdale Trail and Mt Lee Drive",
             file: "InnsdaleMtLee.csv",
-            pos: {lat: 34.1301075672801,lng: -118.3269378549816}//34.1301075672801,-118.3269378549816
+            pos: {lat: 34.1301075672801,lng: -118.3269378549816}, //34.1301075672801,-118.3269378549816
+            alltrails: "https://www.alltrails.com/trail/us/california/innsdale-drive-and-mount-lee-to-the-hollywood-sign?u=i&sh=sqfqqm"
         } 
     ];
     const [selectedOption, setSelectedOption] = useState(0); // Initial selected option
@@ -40,7 +43,7 @@ function Mainframe(){
         <hr></hr>
         <MyHikingMap csv={choices[selectedOption].file} mark={choices[selectedOption].pos}/>
         <hr></hr>
-        <h3><a href="https://www.alltrails.com/parks/us/california/griffith-park">More trails ...</a></h3>
+        <h4><a href={choices[selectedOption].alltrails}>More on Alltrails ...</a></h4>
         <p><a href="https://kcfamilyjourney.github.io/home/">Home</a> | <a href="https://forms.gle/ErqZysKrBFxnL5SV7">Contact Us</a></p>
     </>;
 }
